c18d7b15c91505364b6c2803aa59f6380d032918
JessicaRodriguesdc <jessicarodridc@gmail.com>
2020-01-21 16:51:36 -0300

    add barra de pesquisa e pag dos dev

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,9 @@
 import React, {useState,useEffect}from 'react';
-import { StyleSheet ,Image, View, Text} from 'react-native';
+import { StyleSheet ,Image, View, Text, TextInput,TouchableOpacity} from 'react-native';
 import MapView, {Marker, Callout} from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
-function Main(){
+import { MaterialIcons } from '@expo/vector-icons';
+function Main({navigation}){
     const [currentRegion , setCurrentRegion] = useState(null);
     useEffect(()=>{
         async function loadInitialPosition(){
@@ -32,18 +33,36 @@ function Main(){
         return null;
     }
     return(
-        <MapView initialRegion={currentRegion} style={styles.map}>
-            <Marker coordinate={{ latitude: -3.8351174 , longitude:-38.6213128}}>
-                <Image style={styles.avatar} source={{ uri:'https://avatars3.githubusercontent.com/u/41476072?s=460&v=4' }}/>
-                <Callout>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Jessica Rodrigues</Text>
-                        <Text style={styles.devBio}>blabla</Text>
-                        <Text style={styles.devTechs}>tecs</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView initialRegion={currentRegion} style={styles.map}>
+                <Marker coordinate={{ latitude: -3.8351174 , longitude:-38.6213128}}>
+                    <Image style={styles.avatar} source={{ uri:'https://avatars3.githubusercontent.com/u/41476072?s=460&v=4' }}/>
+                    <Callout onPress={()=>{
+                        navigation.navigate('Profile', { githb_username: 'jessicaRodriguesdc'});
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Jessica Rodrigues</Text>
+                            <Text style={styles.devBio}>blabla</Text>
+                            <Text style={styles.devTechs}>tecs</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                    <TextInput 
+                        style={styles.searchInput}
+                        placeholder="Buscar devs por techs..."
+                        placeholderTextColor="#999"
+                        autoCapitalize="words"
+                        autoCorrect={false}
+                    />
+                    <TouchableOpacity onPress={()=>{}} style={styles.loadButton}>
+                        <MaterialIcons name="my-location" size={20} color={"#FFF"}/>
+                    </TouchableOpacity>
+
+            </View>
+
+        </>
     );
 }
 
