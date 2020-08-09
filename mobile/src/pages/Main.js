0c27010ff4d913eec5b71d447a3cf2d11af876ac
Jéssica Rodrigues da Costa <jessicarodridc@gmail.com>
2020-08-09 17:25:48 -0300

    modificacoes da parte mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -3,7 +3,9 @@ import { StyleSheet ,Image, View, Text, TextInput,TouchableOpacity} from 'react-
 import MapView, {Marker, Callout} from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 import { MaterialIcons } from '@expo/vector-icons';
+
 import api from '../services/api';
+import {connect,disconnect} from '../services/socket';
 
 function Main({navigation}){
     const [devs, setDevs] = useState([]);
@@ -33,6 +35,10 @@ function Main({navigation}){
         loadInitialPosition();
     },[]);
 
+    function setupWebSocket(){
+        connect();
+    }
+
     async function loadDevs(){
         const { latitude, longitude } = currentRegion;
 
@@ -45,6 +51,7 @@ function Main({navigation}){
            
         });
         setDevs(response.data.devs);
+        setupWebSocket();
     }
 
     function handleRegionChanged(region){
--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const api = axios.create({
 
     //baseURL: 'http://10.0.2.2:3333',
-    baseURL: 'http://192.168.1.9:3333',
+    //baseURL: 'http://192.168.1.9:3333',
+    baseURL: 'http://192.168.15.11:19000',
 });
 
 export default api;
